949ed9169383f813059cf201b2a7802312e4bc19
Tom Wheeler <racecarparts@users.noreply.github.com>
2022-11-22 17:22:52 -0700

    markup calendar

--- public/static/js/calendar.js
+++ public/static/js/calendar.js
@@ -1,103 +1,90 @@
 const calendarInterval = 3600000 // every hour
 
-function writeCalendar() {
-    // let text = calData.title + "<br>";
-    // text += calData.cal
+function generateCalendar(d, id) {
+  function monthDays(month, year) {
+    var result = [];
+    var days = new Date(year, month, 0).getDate();
+    for (var i = 1; i <= days; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  Date.prototype.monthDays = function() {
+    var d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+    return d.getDate();
+  };
+  var details = {
+    // totalDays: monthDays(d.getMonth(), d.getFullYear()),
+    totalDays: d.monthDays(),
+    weekDays: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  };
 
-    $(document).ready(function() {
-        var currentDate = new Date();
-        function generateCalendar(d) {
-          function monthDays(month, year) {
-            var result = [];
-            var days = new Date(year, month, 0).getDate();
-            for (var i = 1; i <= days; i++) {
-              result.push(i);
-            }
-            return result;
+  var start = new Date(d.getFullYear(), d.getMonth()).getDay();
+  var cal = [];
+
+  var day = 1;
+  var now = new Date();
+  for (var i = 0; i <= 6; i++) {
+    cal.push(['<tr>']);
+    for (var j = 0; j < 7; j++) {
+      if (i === 0) {
+        cal[i].push('<td>' + details.weekDays[j] + '</td>');
+      } else if (day > details.totalDays) {
+        cal[i].push('<td>&nbsp;</td>');
+      } else {
+        if (i === 1 && j < start) {
+          cal[i].push('<td>&nbsp;</td>');
+        } else {
+          var todaySpan = '<span class="'
+          if (d.getMonth() === now.getMonth() && day === now.getDate()) {
+            todaySpan = todaySpan + 'text-dark bg-light';
           }
-          Date.prototype.monthDays = function() {
-            var d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
-            return d.getDate();
-          };
-          var details = {
-            // totalDays: monthDays(d.getMonth(), d.getFullYear()),
-            totalDays: d.monthDays(),
-            weekDays: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-            months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-          };
-          var start = new Date(d.getFullYear(), d.getMonth()).getDay();
-          var cal = [];
-          var day = 1;
-          for (var i = 0; i <= 6; i++) {
-            cal.push(['<tr>']);
-            for (var j = 0; j < 7; j++) {
-              if (i === 0) {
-                cal[i].push('<td>' + details.weekDays[j] + '</td>');
-              } else if (day > details.totalDays) {
-                cal[i].push('<td>&nbsp;</td>');
-              } else {
-                if (i === 1 && j < start) {
-                  cal[i].push('<td>&nbsp;</td>');
-                } else {
-                  cal[i].push('<td class="day">' + day++ + '</td>');
-                }
-              }
-            }
-            cal[i].push('</tr>');
-          }
-          cal = cal.reduce(function(a, b) {
-            return a.concat(b);
-          }, []).join('');
-          $('.calendar-table').append(cal);
-          $('#month').text(details.months[d.getMonth()]);
-          $('#year').text(d.getFullYear());
-          $('td.day').mouseover(function() {
-            $(this).addClass('hover');
-          }).mouseout(function() {
-            $(this).removeClass('hover');
-          });
+          todaySpan = todaySpan + '">' + day++ + '</span>'
+          cal[i].push('<td class="day">' + todaySpan + '</td>');
         }
-        $('#left').click(function() {
-          $('.calendar-table').text('');
-          if (currentDate.getMonth() === 0) {
-            currentDate = new Date(currentDate.getFullYear() - 1, 11);
-            generateCalendar(currentDate);
-          } else {
-            currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
-            generateCalendar(currentDate);
-          }
-        });
-        $('#right').click(function() {
-          $('.calendar-table').html('<tr></tr>');
-          if (currentDate.getMonth() === 11) {
-            currentDate = new Date(currentDate.getFullYear() + 1, 0);
-            generateCalendar(currentDate);
-          } else {
-            currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1)
-            generateCalendar(currentDate);
-          }
-        });
-        generateCalendar(currentDate);
-      });
-    
+      }
+    }
+    cal[i].push('</tr>');
+  }
 
-    writeWidget('calendar', calendar)
+  // month header
+  cal.unshift([
+    '<tr><td class="text-center" colspan="7">' + 
+    details.months[d.getMonth()] +
+    '</td></tr>'
+  ]);
+
+  cal = cal.reduce(function(a, b) {
+    return a.concat(b);
+  }, []).join('');
+  $('#' + id).append(cal);
 }
 
-function loadCalendar() {
-    // fetch('/calendar', {
-    //     method: 'get'
-    // })
-    //     .then(r => r.json())
-    //     .then(jsonData => {
-             writeCalendar()
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
+function writeCalendar() {
+    var currentDate = new Date();
+
+    if (currentDate.getMonth() === 0) {
+      currentDate = new Date(currentDate.getFullYear() - 1, 11);
+      // generateCalendar(currentDate, 'prev');
+    } else {
+      currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
+      // generateCalendar(currentDate);
+    }
+    generateCalendar(currentDate, 'calendar-table-prev');
+
+    currentDate = new Date();
+    generateCalendar(currentDate, 'calendar-table-cur');
+
+    if (currentDate.getMonth() === 11) {
+      currentDate = new Date(currentDate.getFullYear() + 1, 0);
+    } else {
+      currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1)
+    }
+    generateCalendar(currentDate, 'calendar-table-next');
 }
 
-loadCalendar()
+writeCalendar()
 setInterval(() => {
-    loadCalendar()
+  writeCalendar()
 }, calendarInterval)
